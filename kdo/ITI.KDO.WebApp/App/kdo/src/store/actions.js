dabd07b8f2bbf75043888e47c54920c592b8a06c
VU Tuan Trung <trung.lespace@gmail.com>
2021-01-28 11:51:21 +0100

    group actions methods and export

--- kdo/ITI.KDO.WebApp/App/kdo/src/store/actions.js
+++ kdo/ITI.KDO.WebApp/App/kdo/src/store/actions.js
@@ -1,45 +1,48 @@
 import * as types from './mutation-types';
 
-/**
- * Notify when an error occured
- * @param {*} context
- * @param {*} error
- */
-export function notifyError(context, error) {
-  context.commit(
-    types.ERROR_HAPPENED,
-    `${error.status}: ${error.responseText || error.statusText}`
-  );
-}
+export default {
+  /**
+   * Notify when an error occured
+   * @param {*} context
+   * @param {*} error
+   */
+  notifyError(context, error) {
+    context.commit(
+      types.ERROR_HAPPENED,
+      `${error.status}: ${error.responseText || error.statusText}`
+    );
+  },
 
-/**
- * Notify when an action is loading
- * @param {*} context
- * @param {*} isLoading
- */
-export function notifyLoading(context, isLoading) {
-  context.commit(types.SET_IS_LOADING, isLoading);
-}
+  /**
+   * Notify when an action is loading
+   * @param {*} context
+   * @param {*} isLoading
+   */
+  notifyLoading(context, isLoading) {
+    context.commit(types.SET_IS_LOADING, isLoading);
+  },
 
-const errorMessage = error =>
-  `${error.status}: ${error.responseText || error.statusText}`;
+  errorMessage(error) {
+    return `${error.status}: ${error.responseText || error.statusText}`;
+  },
 
-/**
- * Wraps the async call to an api service in order to handle loading, and errors.
- * @param {*} commit
- * @param {*} apiCall
- * @param {*} rethrowError
- */
-async function wrapAsyncApiCall(commit, apiCall, rethrowError) {
-  commit(types.SET_IS_LOADING, true);
+  /**
+   * Wraps the async call to an api service in order to handle loading, and errors.
+   * @param {*} commit
+   * @param {*} apiCall
+   * @param {*} rethrowError
+   */
+  async wrapAsyncApiCall(commit, apiCall, rethrowError) {
+    commit(types.SET_IS_LOADING, true);
 
-  try {
-    return await apiCall();
-  } catch (error) {
-    commit(types.ERROR_HAPPENED, errorMessage(error));
+    try {
+      return await apiCall();
+    } catch (error) {
+      commit(types.ERROR_HAPPENED, this.errorMessage(error));
 
-    if (rethrowError) throw error;
-  } finally {
-    commit(types.SET_IS_LOADING, false);
+      if (rethrowError) throw error;
+    } finally {
+      commit(types.SET_IS_LOADING, false);
+    }
   }
-}
+};
